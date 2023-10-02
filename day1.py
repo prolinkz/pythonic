f40a526c3415543c58f569ca25fd291a5a23d723
Zakiullah Khan <prolinkzee@gmail.com>
2023-10-03 01:20:15 +0500

    day1.py
    Work  with Strings, Operators

---
+++ day1.py
@@ -0,0 +1,33 @@
+#You can add 2 digits or variable values in Python using the following code:
+
+# define the variables
+num1 = 5
+num2 = 10
+
+# add the variables
+sum = num1 + num2
+
+# print the result
+print("The sum of the two numbers is:", sum)
+
+
+print('''
+        In this code, we define two variables `num1` and `num2` and 
+      assign them the values 5 and 10, respectively. 
+      We then add the values of these variables and 
+      store the result in a new variable called `sum`. 
+      Finally, we print the result.
+    ''')
+
+
+#String and Vairable
+name = "John"
+age = 30
+print(f"My name is {name} and I am {age} years old.")
+
+
+#String Concatenation - +
+
+
+
+#String Interpolation - curly braces {}
